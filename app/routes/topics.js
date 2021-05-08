a6acfb9f9918ebc9f324aedb0941c6cc20d7d624
caiyue.yue <caiyue.yue@bytedance.com>
2021-05-08 19:21:37 +0800

    new change

--- app/routes/topics.js
+++ app/routes/topics.js
@@ -12,7 +12,8 @@ const { secret } = require('../config');
 const auth = jwt({ secret });
 
 router.get('/', find);//所有话题
-router.post('/', auth, create);//创建话题
+// router.post('/', auth, create);//创建话题
+router.post('/', create);//创建话题
 router.get('/:id', checkTopicExist, findById);//获取某个话题
 router.patch('/:id', auth, checkTopicExist, update);//更新话题
 router.get('/:id/followers', checkTopicExist, listFollowers);//话题的粉丝
